aa871610d53d6a268951891e70e14b15e87795b5
kamana999 <49669967+kamana999@users.noreply.github.com>
2020-09-29 12:34:36 +0530

    models

--- learn/cws/models.py
+++ learn/cws/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
 class Category(models.Model):
@@ -9,3 +11,38 @@ class Category(models.Model):
     def __str__(self):
         return self.cat_title
     
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructor = models.CharField(max_length=200)
+    duration = models.DateTimeField()
+    type = models.CharField()
+    status = models.BooleanFeild()
+    price = models.FloatFeidl()
+    course_slug = models.SlugField()
+    category = models.ForeignKey('Category', related_name='', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    ordered = models.BooleanFeild()
+    items = models.ForeignKey(Course)
+    doc = DateTimeField() 
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    ref_code = models.CharField(max_length=15)
+    ordered = models.BooleanFeild()
+    items = models.ManyToManyField(OrderItem)
+    starting_date = models.DateTimeField()
+    ordered_date = models.DateTimeField()
+    status = models.BooleanFeild()
+
+
+
+
+
+
