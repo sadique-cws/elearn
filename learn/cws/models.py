f7bb4cacd2172eaf364a799ae2cb8dab802c5cf0
sadique-cws <sadique.hussain96@gmail.com>
2020-09-29 12:02:50 +0530

    category model added

--- learn/cws/models.py
+++ learn/cws/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    cat_title = models.CharField(max_length=200)
+    cat_slug = models.SlugField()
+    cat_description = models.TextField()
+
+    def __str__(self):
+        return self.cat_title
+    
