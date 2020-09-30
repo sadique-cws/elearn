c5d1ab3c7cf67982e6cc6fbaa35f9f774d21084f
sadique-cws <sadique.hussain96@gmail.com>
2020-09-30 13:10:21 +0530

    model fixed

--- learn/cws/models.py
+++ learn/cws/models.py
@@ -14,9 +14,11 @@ class Course(models.Model):
     description = models.TextField()
     instructor = models.CharField(max_length=200)
     duration = models.DateTimeField()
+    image = models.ImageField(upload_to="course/")
     types = models.CharField(max_length=20)
-    status = models.BooleanField()
+    status = models.BooleanField(default=True)
     price = models.FloatField()
+    discount_price = models.FloatField(null=True)
     course_slug = models.SlugField()
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
@@ -33,14 +35,19 @@ class OrderItem(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     ref_code = models.CharField(max_length=15)
-    ordered = models.BooleanField()
+    ordered = models.BooleanField(default=False)
     items = models.ManyToManyField(OrderItem)
     starting_date = models.DateTimeField()
     ordered_date = models.DateTimeField()
-    status = models.BooleanField()
 
 
 
+class Coupon(models.Model):
+    code = models.CharField(max_length=10)
+    amount = models.FloatField()
+    status = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.code
 
-
+    
